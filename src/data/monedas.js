4c7daf256137c085770aaee26fe767a67089caef
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-10-08 11:11:29 -0300

    peso chileno deleted

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -1,6 +1,5 @@
 const monedas = [
     {id: "USD", nombre: "Dolar de Estados Unidos"},
-    {id: "CLP", nombre: "Peso Chileno"},
     {id: "EUR", nombre: "Euro"},
     {id: "GBP", nombre: "Libra Esterlina"},
 ]
